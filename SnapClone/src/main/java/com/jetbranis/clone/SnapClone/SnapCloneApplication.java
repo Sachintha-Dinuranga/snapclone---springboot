6a165d6825089876b56f02dd28432cda62876350
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-03-07 12:35:39 +0530

    Initial commit

---
+++ SnapClone/src/main/java/com/jetbranis/clone/SnapClone/SnapCloneApplication.java
@@ -0,0 +1,13 @@
+package com.jetbranis.clone.SnapClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapCloneApplication.class, args);
+	}
+
+}
---
+++ SnapClone/src/test/java/com/jetbranis/clone/SnapClone/SnapCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbranis.clone.SnapClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
